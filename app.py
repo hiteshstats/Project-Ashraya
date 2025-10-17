d0956bfae3a14a7b6284b7cfa893672bbe038bdd
Hitesh <128674382+hiteshstats@users.noreply.github.com>
2025-10-17 22:00:57 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-  GNU nano 8.5                                                                       app.py
 from flask import Flask, render_template, request
 import smtplib
 from email.mime.text import MIMEText
+import os
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 # Configuration
-YOUR_EMAIL = "hitesh.bagade9096@gmail.com"
-YOUR_PASSWORD = "kmom iamq qqcw ztee"  # Use Gmail App Password, not your real password
+YOUR_EMAIL = os.getenv("YOUR_EMAIL")
+YOUR_PASSWORD = os.getenv("YOUR_PASSWORD")
 
-@app.route("/", methods=["GET", "POST"])
+@application.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         favourite = request.form.get("favourite")
@@ -21,13 +21,10 @@ def send_email(favourite):
     subject = "New Favourite Thing of the Month  "
     body = f"Your girlfriend’s favourite thing for this month is: {favourite}"
     msg = MIMEText(body)
-    msg["Subject"] = "Ashraya's new favourite of the month  "
+    msg["Subject"] = "Ashraya's new favourite of the month 💖 "
     msg["From"] = YOUR_EMAIL
     msg["To"] = YOUR_EMAIL
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(YOUR_EMAIL, YOUR_PASSWORD)
         server.send_message(msg)
-
-if __name__ == "__main__":
-    app.run(debug=True)
