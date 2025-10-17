090bf200a16dd894acbbef881ef5a3f067e6449a
Hitesh <128674382+hiteshstats@users.noreply.github.com>
2025-10-18 00:01:11 +0530

    Updated application.py

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ def home():
 
 def send_email(favourite):
     subject = "New Favourite Thing of the Month  "
-    body = f"Your girlfriend’s favourite thing for this month is: {favourite}"
+    body = f"Ashraya's favourite thing for this month is : {favourite}"
     msg = MIMEText(body)
     msg["Subject"] = "Ashraya's new favourite of the month 💖 "
     msg["From"] = YOUR_EMAIL
